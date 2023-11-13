6cf30e0a12e97d3f0bc4046f9c4873f4e814e369
Kareem Aboulhosn <aboulhosnk@vcu.edu>
2023-11-12 21:36:45 -0500

    Commit for HW7

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # cmsc508-fa2023-hw7-template
+Homework 7 - Creating a Resume Database
+
+This repository contains the solution for Homework 7, which involves creating a resume database using SQL.
+
+Project Description
+This project involves a series of SQL queries and tasks related to database management and manipulation. The tasks involve creating and populating tables, querying data, and analyzing relationships between different data entities. The document is structured gradually, guiding you through each task with clear instructions and expected outcomes.
+
+Setup
+To run the code, you need to have Python installed along with the following libraries:
+
+pandas
+tabulate
+dotenv
+sqlalchemy
+You can install these libraries using pip
+
+Configuration
+You need to set up the following environment variables:
+
+CMSC508_USER: Your database username
+CMSC508_PASSWORD: Your database password
+CMSC508_HOST: Your database host
+You can set these variables in a .env file in the root of the project.
+
+Running the Code
+You can run the code in a Jupyter notebook or any Python IDE. Make sure to set your working directory to the root of the project.
+
--- hw7/hw7-ddl.sql
+++ hw7/hw7-ddl.sql
@@ -17,7 +17,11 @@
 # Drops all tables.  This section should be amended as new tables are added.
 
 SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS peopleroles;
+DROP TABLE IF EXISTS peopleskills;
 DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS roles;
 # ... 
 SET FOREIGN_KEY_CHECKS=1;
 
@@ -28,12 +32,30 @@ SET FOREIGN_KEY_CHECKS=1;
 # time committment offers some sense of how much time was required (or will be required) to gain the skill.
 # You can assign the skill descriptions.  Please be creative!
 
+CREATE TABLE skills (
+    id int not null,
+    skills_name varchar(255) not null,
+    skills_desc varchar(255) not null,
+    skills_tag varchar(255) not null,
+    skills_url varchar(255),
+    skills_time_commitment int,
+    primary key (id)
+);
 
 # Section 3
 # Populate skills
 # Populates the skills table with eight skills, their tag fields must exactly contain “Skill 1”, “Skill 2”, etc.
 # You can assign skill names.  Please be creative!
 
+INSERT INTO skills(id, skills_name, skills_desc, skills_tag, skills_url, skills_time_commitment) values
+(1, 'Python Programming', 'Ability to write software in Python', 'Programming', 'https://www.python.org/', 200),
+(2, 'Data Analysis', 'Ability to analyze data using statistical methods', 'Data Science', 'https://www.datascience.com/', 150),
+(3, 'Web Design', 'Ability to design user-friendly websites', 'Design', 'https://www.example.com/web-design-certification', 100),
+(4, 'Machine Learning', 'Ability to create predictive models', 'Data Science', 'https://www.example.com/machine-learning-certification', 300),
+(5, 'Database Management', 'Ability to manage SQL databases', 'Database', 'https://www.example.com/database-management-certification', 120),
+(6, 'JavaScript Programming', 'Ability to write software in JavaScript', 'Programming', 'https://www.example.com/javascript-certification', 180),
+(7, 'Project Management', 'Ability to manage projects effectively', 'Management', 'https://www.example.com/project-management-certification', 240),
+(8, 'Graphic Design', 'Ability to create visually appealing designs', 'Design', 'https://www.example.com/graphic-design-certification', 160);
 
 # Section 4
 # Create people( id,first_name, last_name, email, linkedin_url, headshot_url, discord_handle, brief_bio, date_joined)
@@ -41,8 +63,15 @@ SET FOREIGN_KEY_CHECKS=1;
 # All other fields can default to NULL.
 
 CREATE TABLE people (
-    people_id int,
-    people_last_name varchar(256) NOT NULL,
+    people_id int NOT NULL,
+    people_first_name varchar(255),
+    people_last_name varchar(255) NOT NULL,
+    people_email varchar(255),
+    people_linkedin_url varchar(255),
+    people_headshot_url varchar(255),
+    people_discord_handle varchar(255),
+    people_brief_bio varchar(4096),
+    people_date_joined date NOT NULL,
     PRIMARY KEY (people_id)
 );
 
@@ -51,13 +80,33 @@ CREATE TABLE people (
 # Their last names must exactly be “Person 1”, “Person 2”, etc.
 # Other fields are for you to assign.
 
-insert into people (people_id,people_last_name) values (1,'Person 1');
+INSERT INTO people (people_id, people_first_name, people_last_name, people_email, people_linkedin_url, people_headshot_url, people_discord_handle, people_brief_bio, people_date_joined) VALUES 
+(1, 'John', 'Person 1', 'john@example.com', 'https://www.linkedin.com/in/john', 'https://example.com/john.jpg', 'john#1234', 'I am John', '2022-01-01'),
+(2, 'Jane', 'Person 2', 'jane@example.com', 'https://www.linkedin.com/in/jane', 'https://example.com/jane.jpg', 'jane#1234', 'I am Jane', '2022-01-02'),
+(3, 'Bob', 'Person 3', 'bob@example.com', 'https://www.linkedin.com/in/bob', 'https://example.com/bob.jpg', 'bob#1234', 'I am Bob', '2022-01-03'),
+(4, 'Alice', 'Person 4', 'alice@example.com', 'https://www.linkedin.com/in/alice', 'https://example.com/alice.jpg', 'alice#1234', 'I am Alice', '2022-01-04'),
+(5, 'Charlie', 'Person 5', 'charlie@example.com', 'https://www.linkedin.com/in/charlie', 'https://example.com/charlie.jpg', 'charlie#1234', 'I am Charlie', '2022-01-05'),
+(6, 'Eve', 'Person 6', 'eve@example.com', 'https://www.linkedin.com/in/eve', 'https://example.com/eve.jpg', 'eve#1234', 'I am Eve', '2022-01-06'),
+(7, 'David', 'Person 7', 'david@example.com', 'https://www.linkedin.com/in/david', 'https://example.com/david.jpg', 'david#1234', 'I am David', '2022-01-07'),
+(8, 'Grace', 'Person 8', 'grace@example.com', 'https://www.linkedin.com/in/grace', 'https://example.com/grace.jpg', 'grace#1234', 'I am Grace', '2022-01-08'),
+(9, 'Oliver', 'Person 9', 'oliver@example.com', 'https://www.linkedin.com/in/oliver', 'https://example.com/oliver.jpg', 'oliver#1234', 'I am Oliver', '2022-01-09'),
+(10, 'Sophie', 'Person 10', 'sophie@example.com', 'https://www.linkedin.com/in/sophie', 'https://example.com/sophie.jpg', 'sophie#1234', 'I am Sophie', '2022-01-10');
 
 
 # Section 6
 # Create peopleskills( id, skills_id, people_id, date_acquired )
-# None of the fields can ba NULL. ID can be auto_increment.
+# None of the fields can be NULL. ID can be auto_increment.
 
+create table peopleskills(
+    id int auto_increment,
+    skills_id int not null,
+    people_id int not null,
+    date_acquired date default (current_date),
+    primary key (id),
+    foreign key (skills_id) references skills (id),
+    foreign key (people_id) references people (people_id),
+    unique (skills_id, people_id)
+);
 
 # Section 7
 # Populate peopleskills such that:
@@ -73,25 +122,55 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 10 has skills 1,4,5;
 # Note that no one has yet acquired skills 7 and 8.
  
+ insert into peopleskills(people_id, skills_id) values
+ (1,1), (1,3), (1,6),
+ (2,3), (2,4), (2,5),
+ (3,1), (3,5),
+ (5,3), (5,6),
+ (6,2), (6,3), (6,4),
+ (7,3), (7,5), (7,6),
+ (8,1), (8,3), (8,5), (8,6),
+ (9,2), (9,5), (9,6),
+ (10, 1), (10,4), (10,5);
+
 
 # Section 8
 # Create roles( id, name, sort_priority )
 # sort_priority is an integer and is used to provide an order for sorting roles
 
-
+CREATE TABLE roles (
+    id int not null,
+    name varchar(255) not null,
+    sort_priority int not null,
+    primary key (id)
+);
 
 # Section 9
 # Populate roles
 # Designer, Developer, Recruit, Team Lead, Boss, Mentor
 # Sort priority is assigned numerically in the order listed above (Designer=10, Developer=20, Recruit=30, etc.)
 
-
+INSERT INTO roles(id, name, sort_priority) values
+(1, 'Designer', 10),
+(2, 'Developer', 20),
+(3, 'Recruit', 30),
+(4, 'Team Lead', 40),
+(5, 'Boss', 50),
+(6, 'Mentor', 60);
 
 # Section 10
 # Create peopleroles( id, people_id, role_id, date_assigned )
 # None of the fields can be null.  ID can be auto_increment
 
-
+CREATE TABLE peopleroles (
+    id int auto_increment,
+    people_id int not null,
+    role_id int not null,
+    date_assigned date default (current_date),
+    primary key (id),
+    foreign key (people_id) references people (people_id),
+    foreign key (role_id) references roles (id)
+);
 
 # Section 11
 # Populate peopleroles
@@ -106,3 +185,14 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 is Developer
 # Person 10 is Developer and Designer
 
+INSERT INTO peopleroles(people_id, role_id) values
+(1, 2),
+(2, 5), (2, 6),
+(3, 2), (3, 4),
+(4, 3),
+(5, 3),
+(6, 1), (6, 2), (6, 4),
+(7, 1),
+(8, 1), (8, 4), (8, 2), (8, 3),
+(9, 2),
+(10, 1), (10, 2), (10, 4);
